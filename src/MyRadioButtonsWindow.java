f8f8ff89c446287e90f2bca60cf22f15230a1517
alvaropallares20 <alvaropallaresm@gmail.com>
2022-09-16 11:09:28 +0200

    radioButtom

---
+++ src/MyRadioButtonsWindow.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyRadioButtonsWindow {
+
+    public static void main(String[] args) {
+
+        JFrame f = new JFrame();
+        f.setBounds(10, 10, 200, 300);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainpanel = new JPanel();
+        mainpanel.setBackground(Color.pink);
+        mainpanel.setLayout(null);
+
+        JRadioButton boton1 = new JRadioButton();
+        boton1.
+    }
+}
