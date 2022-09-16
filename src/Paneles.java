62ec96af6c8fe1471c67a43244bcf4ae8b59d8cd
alvaropallares20 <alvaropallaresm@gmail.com>
2022-09-16 10:53:41 +0200

    mi primera ventana - dos paneles

--- src/Paneles.java
+++ src/Paneles.java
@@ -5,13 +5,29 @@ public class Paneles {
     public static void main(String[] args) {
 
         JFrame f = new JFrame();
-        f.setBounds(10, 10, 200, 300);
+        f.setBounds(10, 10, 2000, 300);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.RED);
-        panel.setBounds(40, 80, 200, 200);
-        panel.setBounds(40, 80, 200, 200);
+        JPanel mainpanel = new JPanel();
+        mainpanel.setBackground(Color.pink);
+        mainpanel.setLayout(null);
+
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(Color.RED);
+        panel1.setBounds(0, 0, 100, 100);
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.BLUE);
+        panel2.setBounds(200, 0, 100, 100);
+
+        mainpanel.add(panel1);
+        mainpanel.add(panel2);
+
+        f.setContentPane(mainpanel);
+
+        f.revalidate();
+        f.repaint();
+
+
     }
 }
